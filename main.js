59302ada14aba9a98b86362350d077b8a08b7669
Chris Harrison <someuser-321@users.noreply.github.com>
2016-12-06 22:41:12 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -52,8 +52,70 @@ THREE.PointerLockControls = function (camera) {
 
 
 var ticks = 0,
-	fps = 60,
-	fov = 80;
+	fps = 60;
+	
+var config = {
+	rRate : {
+		max: 1,
+		min: 0,
+		val: 1 / 50
+	},
+	bgRate: {
+		max: 1,
+		min: 0,
+		val: 1 / 10
+	},
+	cRate: {
+		max: 1 / 100,
+		min: 0,
+		val: 1 / 800
+	},
+	cThresh: {
+		max: -10,
+		min: -50,
+		val: -35
+	},
+	dispFactor: {
+		max: 5,
+		min: 0,
+		val: 2
+	},
+	dispFactorX: {
+		max: 1,
+		min: 0,
+		val: 1
+	},
+	dispFactorY: {
+		max: 1,
+		min: 0,
+		val: 0.5
+	},
+	dispFactorZ: {
+		max: 1,
+		min: 0,
+		val: 1
+	},
+	minFOV: {
+		max: 90,
+		min: 60,
+		val: 60
+	},
+	maxFOV: {
+		max: 120,
+		min: 90,
+		val: 95
+	},
+	minScale: {
+		max: 4,
+		min: 1,
+		val: 1
+	},
+	maxScale: {
+		max: 1,
+		min: 0,
+		val: 1
+	}
+};
 
 var ctx,
 	audio,
@@ -80,9 +142,12 @@ var nCubesX = 16,
 	nCubesY = 16,
 	nCubesZ = 16;
 	
-var maxX = nCubesX/2,
+var //minX = nCubesX/2,
+	maxX = nCubesX/2,
+	//minY = -4, //nCubesY/2,
 	maxY = nCubesY/2,
-	maxZ = nCubesZ/2;
+	//minZ = -4,
+	maxZ = nCubesZ;
 
 var cubeWidthX = 1/(2*nCubesX),
     cubeWidthY = 1/(2*nCubesY),
@@ -100,7 +165,7 @@ document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 scene.background = new THREE.Color(0, 0, 0);
 
-var camera = new THREE.PerspectiveCamera(fov, window.innerWidth/window.innerHeight, 0.001, 1000);
+var camera = new THREE.PerspectiveCamera((config.minFOV.val+config.maxFOV.val)/2, window.innerWidth/window.innerHeight, 0.001, 1000);
 var controls = new THREE.PointerLockControls(camera);
 controls.getObject().translateZ(maxZ/2);
 controls.getObject().rotateZ(Math.PI/2);
@@ -129,9 +194,9 @@ for ( var i=0 ; i<nCubesX ; i++ ) {
 			});
 			var cube = new THREE.Mesh(cubeGeometry, material);
 			
-			cube.position.x = maxX*((i+0.5)/nCubesX-0.5);
-			cube.position.y = maxY*((j+0.5)/nCubesY-0.5);
-			cube.position.z = maxZ*((k+0.5)/nCubesZ-0.5);
+			cube.position.x = /*minX + (maxX - minX) * ((i+0.5) / nCubesX); */ maxX*((i+0.5)/nCubesX-0.5);
+			cube.position.y = /*minY + (maxY - minY) * ((j+0.5) / nCubesY); */ maxY*((j+0.5)/nCubesY-0.5);
+			cube.position.z = /*minZ + (maxZ - minZ) * (k / nCubesZ); */ maxZ*((k+0.5)/nCubesZ-0.5);
 			
 			if ( !cubes[i] ) {
 				cubes.push([]);
@@ -185,6 +250,34 @@ geometry.vertices.push(
 var lineZ = new THREE.Line(geometry, new THREE.LineBasicMaterial({
 	color: 0x0000ff
 }));
+
+var inpidx = 0;
+var topOffset = 40;
+for ( var a in config ) {
+	if ( config.hasOwnProperty(a) )
+	{
+		var tmpControl = document.createElement("input");
+			tmpControl.type = "range";
+			tmpControl.min = config[a].min;
+			tmpControl.max = config[a].max;
+			tmpControl.step = (config[a].max-config[a].min) / 100;
+			tmpControl.setAttribute("value", config[a].val);
+			tmpControl.id = a;
+			tmpControl.style.left = "100px";
+			tmpControl.style.top = (25*inpidx) + topOffset + "px";
+		
+		var tmpLabel = document.createElement("span");
+			tmpLabel.innerText = a + ":";
+			tmpLabel.style.textAlign = "right";
+			tmpLabel.style.width = "90px";
+			tmpLabel.style.top = (25*inpidx) + 2 + topOffset + "px";
+		
+		document.getElementById("inputs").appendChild(tmpLabel);
+		document.getElementById("inputs").appendChild(tmpControl);
+		
+		inpidx++;
+	}
+}
 
 document.getElementById("audioFile").addEventListener("change", function (){
 	var file = document.getElementById("audioFile").files[0];
@@ -241,6 +334,12 @@ document.addEventListener("keydown", function (e) {
 		case "d":
 			rotVelocity -= 0.01;
 			break;
+		case "x":
+			velocity = 0;
+			break;
+		case "c":
+			rotVelocity = 0;
+			break;
 		case "k":
 			if ( !showAxes )
 			{
@@ -268,8 +367,8 @@ document.addEventListener("wheel", function (e) {
 
 function mouseMove(dx, dy)
 {
-		rotAccX = dx/1000;
-		rotAccY = dy/1000;
+		rotAccX = Math.sign(dx)*Math.sqrt(Math.abs(dx))/2000;
+		rotAccY = Math.sign(dy)*Math.sqrt(Math.abs(dy))/2000;
 }
 
 window.addEventListener('resize', function (){
@@ -295,8 +394,8 @@ var render = function () {
 	rotAccX /= 2;
 	rotAccY /= 2;
 	
-	rotVelX = Math.abs(rotVelX) < 0.0005 ? 0 : rotVelX/1.1
-	rotVelY = Math.abs(rotVelY) < 0.0005 ? 0 : rotVelY/1.1
+	rotVelX = Math.abs(rotVelX) < 0.00005 ? 0 : rotVelX/1.075;
+	rotVelY = Math.abs(rotVelY) < 0.00005 ? 0 : rotVelY/1.075;
 	
 	controls.getObject().rotateX(-rotVelY);
 	controls.getObject().rotateY(-rotVelX);
@@ -304,17 +403,32 @@ var render = function () {
 	
 	light1.position.z = controls.getObject().position.z - 1;
 	
-	var rRate = 2 * fps,
-		bgRate = 1 / 10,
-		cRate = 1 / 800,
-		cThresh = -35,
-		dispFactorX = 1,
-		dispFactorY = 0.5;
+	
+	for ( var a in config ) {
+		if ( config.hasOwnProperty(a) )
+		{
+			config[a].val = parseFloat(document.getElementById(a).value);
+		}
+	}
+	
+	var rRate = config.rRate.val,
+		bgRate = config.bgRate.val,
+		cRate = config.cRate.val,
+		cThresh = config.cThresh.val,
+		dispFactor = config.dispFactor.val,
+		dispFactorX = config.dispFactorX.val,
+		dispFactorY = config.dispFactorY.val,
+		dispFactorZ = config.dispFactorZ.val,
+		minFOV = config.minFOV.val,
+		maxFOV = config.maxFOV.val,
+		minScale = config.minScale.val,
+		maxScale = config.maxScale.val;
 		
 		
 	scene.background.setHSL(bgRate*ticks/fps, 1, 0);
 	
-	if ( analyser ) {
+	if ( analyser && !audio.ended )
+	{
 		analyser.getFloatFrequencyData(frequencyData);
 
 		var fData = new Float32Array(frequencyData.length);
@@ -325,25 +439,25 @@ var render = function () {
 			minPow2 = 1000,
 			maxPow2 = -1000;
 
-		for ( var i=0 ; i<frequencyData.length ; i++ ) {
+		for ( var i=0 ; i<frequencyData.length ; i++ )
+		{
 			minPow1 = Math.min(minPow1, frequencyData[i]);
 			maxPow1 = Math.max(maxPow1, frequencyData[i]);
 			avgPow1 += frequencyData[i] / frequencyData.length;
 		}
 		
-		for ( var i=0 ; i<frequencyData.length ; i++ ) {
+		for ( var i=0 ; i<frequencyData.length ; i++ )
+		{
 			fData[i] = (frequencyData[i]-minPow1)/(maxPow1-minPow1);
 			avgPow2 += fData[i] / fData.length;
 			minPow2 = Math.min(minPow2, fData[i]);
 			maxPow2 = Math.max(maxPow2, fData[i]);
 		}
 	
-		//console.log(avgPow1 + ": " + maxPow1 + ", " + minPow1);
-		//console.log(avgPow2 + ": " + maxPow2 + ", " + minPow2);
-	
 		var avgPowers = [],
 			avgAvgPowers = 0;
-		for ( var i=0 ; i<nCubesX/2 ; i++ ) {
+		for ( var i=0 ; i<nCubesX/2 ; i++ )
+		{
 			var avgPow3 = 0,
 				binLen = (fData.length/(nCubesX/2))
 				llim = i * binLen;
@@ -355,35 +469,28 @@ var render = function () {
 			avgAvgPowers += avgPow3/(nCubesX/2);
 		}
 		
+		
 		if ( maxPow1 > cThresh )
 			cidx += cRate*Math.pow(Math.abs(maxPow1-cThresh), 1.65);
 		
-		var minFOV = 85,
-			maxFOV = 95,
-			minScale = 1,
-			maxScale = 1;
-			
-		camera.fov = maxFOV - avgPowers[0]*(maxFOV-minFOV);
-		camera.updateProjectionMatrix();
 	
-		for ( var i=0 ; i<nCubesX ; i++ ) {
+		for ( var i=0 ; i<nCubesX ; i++ )
+		{
 			var x_ = i/nCubesX,
 				_x = i/nCubesX - 0.5,
 				xidx = 0, //maxFreq*x_,
-				disp = 0,
-				dispFactor = 2;
+				disp = 0;
 			
 			if ( i < nCubesX/2 )
 				xidx = nCubesX/2 - i - 1;
 			else
 				xidx = i - nCubesX/2;
 			
-			
-			
 			disp = dispFactor * avgPowers[xidx];
 			disp = isNaN(disp) ? 0 : disp;
 			
-			for ( var j=0 ; j<nCubesY ; j++ ) {
+			for ( var j=0 ; j<nCubesY ; j++ )
+			{
 				var y_ = j/nCubesY,
 					_y = j/nCubesY - 0.5,
 					yidx = 0,
@@ -394,44 +501,51 @@ var render = function () {
 				else
 					yidx = j - nCubesY/2;
 				
-				for ( var k=0 ; k<nCubesZ ; k++ ) {
-							var cube = cubes[i][j][k];
-							var cube_orig = cubes_orig[i][j][k];
-							
-							var x = cube.position.x,
-								y = cube.position.y,
-								z = cube.position.z;
-							
-							var z_ = k/nCubesZ,
-								_z = k/nCubesZ - 0.5,
-								zidx = 0;
-						
-							if ( k < nCubesZ/2 )
-								zidx = nCubesZ/2 - k - 1;
-							else
-								zidx = k - nCubesZ/2;
-								
-							
-							cube.position.z = cube_orig.z + disp;
-							
-							cube.position.x += disp*dispFactorX * Math.sin(ticks/fps*Math.abs(disp) + 2*Math.PI*yidx/maxY * zidx)/1000;
-							cube.position.y += disp*dispFactorY * Math.sin(Math.sqrt(2)*ticks/fps*Math.abs(disp) + 2*2*Math.PI*xidx/maxX * zidx)/1000;
-							cube.position.z += disp*Math.sin(ticks/fps*Math.abs(disp) + 2*2*Math.PI*zidx)/1000;
-							
-							cube.scale.x =
-							cube.scale.y =
-							cube.scale.z = minScale + dist*avgPowers[xidx]*(maxScale-minScale);
-							
-							cube.material.color.setHSL(cidx+xidx/nCubesX+yidx/nCubesY, cube_orig.s, cube_orig.l);	//((ticks/cRate)%fps)/fps
-							
-							cube.rotation.x = Math.sin(ticks/fps * _y);
-							cube.rotation.y = Math.sin(1.5*ticks/fps * _x + Math.PI/2);
-							
-							//cube.updateMatrix();
+				for ( var k=0 ; k<nCubesZ ; k++ )
+				{
+					var cube = cubes[i][j][k];
+					var cube_orig = cubes_orig[i][j][k];
+					
+					//cube.position.x = cube_orig.x;
+					//cube.position.y = cube_orig.y;
+					//cube.position.z = cube_orig.z;
+					
+					var x = cube.position.x,
+						y = cube.position.y,
+						z = cube.position.z;
+					
+					var z_ = k/nCubesZ,
+						_z = k/nCubesZ - 0.5,
+						zidx = 0;
+				
+					if ( k < nCubesZ/2 )
+						zidx = nCubesZ/2 - k - 1;
+					else
+						zidx = k - nCubesZ/2;
+					
+					cube.position.z = cube_orig.z + disp;
+					
+					cube.position.x += disp * dispFactorX * Math.sin(ticks/fps*Math.abs(disp) + 2*Math.PI*yidx/maxY * zidx)/1000;
+					cube.position.y += disp * dispFactorY * Math.sin(Math.sqrt(2)*ticks/fps*Math.abs(disp) + 2*2*Math.PI*xidx/maxX * zidx)/1000;
+					cube.position.z += disp * dispFactorZ * Math.sin(ticks/fps*Math.abs(disp) + 2*2*Math.PI*zidx)/1000;
+					
+					cube.scale.x =
+					cube.scale.y =
+					cube.scale.z = minScale + Math.abs(dist*avgPowers[xidx]*(maxScale-minScale));
+					
+					cube.material.color.setHSL(cidx+xidx/nCubesX+yidx/nCubesY, cube_orig.s, cube_orig.l);	//((ticks/cRate)%fps)/fps
+					
+					cube.rotation.x = Math.sin(ticks/fps * _y);
+					cube.rotation.y = Math.sin(1.5*ticks/fps * _x + Math.PI/2);
 				}
 			}
 		}
+		
+		camera.fov = maxFOV - avgPowers[0]*(maxFOV-minFOV);
+		camera.updateProjectionMatrix();
+	
 	}
+	
 	renderer.render(scene, camera);
 };
 
